37ca1172c5f1c5e3ecd7bea1ac15eb84ce4e3d20
Victoria Banjo <67071771+banjovic@users.noreply.github.com>
2025-05-16 14:10:10 +0100

    user profile app

---
+++ lib/main.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'User Profile App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey.shade300),
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: <Widget>[
+              SizedBox(
+                height: 250,
+                width: double.infinity,
+                child: Image(
+                  image: AssetImage("assets/21198.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(15, 200, 15, 15),
+                child: Column(
+                  children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(15),
+                          margin: EdgeInsets.only(top: 15),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
+                                margin: EdgeInsets.only(left: 95),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      "Victoria Banjo",
+                                      style: Theme.of(
+                                        context,
+                                      ).textTheme.titleLarge?.copyWith(
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    SizedBox(height: 2),
+                                    Text(
+                                      "Frontend Developer",
+                                      style: Theme.of(
+                                        context,
+                                      ).textTheme.bodyLarge?.copyWith(
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Passionate developer specializing in Flutter and React.",
+                                      style: Theme.of(
+                                        context,
+                                      ).textTheme.bodySmall?.copyWith(
+                                        fontWeight: FontWeight.w300,
+                                        fontStyle: FontStyle.italic,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    child: Column(
+                                      children: <Widget>[
+                                        IconButton(
+                                          onPressed: () {},
+                                          icon: Icon(Icons.flutter_dash),
+                                        ),
+                                        Text("Flutter"),
+                                      ],
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Column(
+                                      children: <Widget>[
+                                        IconButton(
+                                          onPressed: () {},
+                                          icon: Icon(Icons.code),
+                                        ),
+                                        Text("Typescript"),
+                                      ],
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Column(
+                                      children: <Widget>[
+                                        IconButton(
+                                          onPressed: () {},
+                                          icon: Icon(Icons.movie),
+                                        ),
+                                        Text("Anime"),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 80,
+                            height: 80,
+                            margin: EdgeInsets.only(left: 15),
+                            decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  offset: Offset(0, 0),
+                                  blurRadius: 10,
+                                  color: Colors.black.withOpacity(0.15),
+                                ),
+                              ],
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                image: AssetImage("assets/profile.png"),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          ListTile(title: Text("Contact Information")),
+                          Divider(),
+                          ListTile(
+                            title: Text("Email"),
+                            subtitle: Text("banjovictoria25@gmail.com"),
+                            leading: Icon(Icons.email),
+                          ),
+                          ListTile(
+                            title: Text("Instagram"),
+                            subtitle: Text(
+                              "https://www.instagram.com/_banjovictoria/",
+                            ),
+                            leading: Icon(LineIcons.instagram),
+                          ),
+                          ListTile(
+                            title: Text("About"),
+                            subtitle: Text(
+                              "pushing boundaries and delivering results...",
+                            ),
+                            leading: Icon(Icons.format_align_center),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
